4de327f4ac98fe20bf3a0dd9608811d83672d598
VanajD <159915531+VanajD@users.noreply.github.com>
2024-06-19 22:03:46 +0530

    Add files via upload

---
+++ Cl1_1.java
@@ -0,0 +1,28 @@
+package day1_1;
+
+interface interface1_1{
+	void function();
+}
+
+public class Cl1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bytevalue=127;
+		System.out.println("bytevalue:"+bytevalue);
+		short shortvalue = 32767;
+		System.out.println("short value:"+shortvalue);
+		int intvalue=2147483647;
+		System.out.println("int value:"+intvalue);
+		float floatvalue=3.14f;
+		System.out.println("float value:"+floatvalue);
+		long longvalue = 9223372036854775807L;
+		System.out.println("long value:"+longvalue);
+		double doublevalue=3.141592653589793;
+		System.out.println("double value"+doublevalue);
+		char charvalue='a';
+		System.out.println("char value:"+charvalue);
+		
+	}
+
+}
