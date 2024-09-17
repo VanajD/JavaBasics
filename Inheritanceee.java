1e725d8166127f6ea1a0b1e47232ea43edb2d85d
VanajD <159915531+VanajD@users.noreply.github.com>
2024-09-17 11:31:32 +0530

    Add files via upload

---
+++ Inheritanceee.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class Inheritanceee extends CallmtdwithoutParas{
+
+	public static void main(String[] ars) {
+		Inheritanceee i =new Inheritanceee();
+		i.addition();
+		i.subtraction();
+	}
+
+}
