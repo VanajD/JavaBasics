d1dbb2e230491872a66b5862276540f93c8a1cbd
VanajD <159915531+VanajD@users.noreply.github.com>
2024-09-27 19:26:29 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,10 @@
+package statics.pack;
+
+public class StaticBlock {
+	static {
+		System.out.println("it is static block....");	//it is executed when class is loaded into jvm  no other ways
+	}
+	public static void main(String[] args) {
+	}
+
+}
