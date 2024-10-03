fb05e24c1334794364dfae6d09cd380322ed8b69
VanajD <159915531+VanajD@users.noreply.github.com>
2024-10-03 21:47:10 +0530

    Add files via upload

---
+++ Employee1_Constructor.java
@@ -0,0 +1,30 @@
+package constructors;
+
+public class Employee1 {
+	Employee1(){ //Constructor created whose name is same as its class
+		System.out.println("default constructor");
+	}
+	public static void main(String[] args) {
+		new Employee1();//constructor will call while object creation
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
